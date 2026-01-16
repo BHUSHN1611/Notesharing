7a628205312d899b4f2da7ddb8c3a15853dd9e0d
BHUSHN1611 <bhushanjadhav1603@gmail.com>
2026-01-16 20:26:32 +0530

    allow frontend to make req

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,6 +11,7 @@ app.use(express.static("public"))
 // cors configuration 
 app.use(cors({
   origin: ['https://notesharing-frontend-a50k.onrender.com/', 'http://localhost:5173'],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }))
 
