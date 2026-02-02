ff40e36a254b5c0f9c031cce7b42e62399462515
BHUSHN1611 <bhushanjadhav1603@gmail.com>
2026-02-02 21:25:15 +0530

    fix: update SameSite attribute in cookie options for improved security

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -88,7 +88,7 @@ const loginUser = asyncHandler(async(req,res)=>{
      const options = {
         httpOnly : true,
         secure: true,
-        sameSite: "none",    
+        sameSite: "lax",    
         maxAge: 7 * 24 * 60 * 60 * 1000 
      }
      return res
