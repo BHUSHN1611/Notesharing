ce7798ea89eae3cb57d804099a016feb60844659
BHUSHN1611 <bhushanjadhav1603@gmail.com>
2026-02-02 21:02:26 +0530

    fix: correct API_URL assignment to ensure proper backend URL usage

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
-export const API_URL = 'http://localhost:8000';
+export const API_URL = 'https://notesharing-backend-cdol.onrender.com' ||'http://localhost:8000';
 
 
-//  'https://notesharing-backend-cdol.onrender.com' ||
+//  
 
 
